9c252f3bc41a1cd4921adefca582ba5c3a719c30
lapequemochi <lapequemochi7@gmail.com>
2024-03-10 18:50:04 -0500

    first commit

---
+++ LineaTelefonica.py
@@ -0,0 +1,94 @@
+class LineaTelefonica:
+    '''----------------------------------------------------------------
+    # atributos
+    ----------------------------------------------------------------'''
+    
+    # Numero de llamadas realizadas
+    numeroLlamadas=""
+    
+    # Numero de minutos consumidos
+    numeroMinutos=""
+    
+    # Costo total de las llamadas
+    costoLlamadas=""
+    
+    '''----------------------------------------------------------------
+    # Metodos
+    ----------------------------------------------------------------'''
+    
+    # Inicializar el número de llamadas, número de minutos y costo de llamadas en 0.
+    def __init__(self):
+        # TODO Parte2 PuntoA: Completar el método según la documentación dada.
+        #Aqui va el codigo
+        numeroLlamadas =0
+        numeroMinutos= 0
+        costoLlamadas=0
+
+    #Retorna el costo total de las llamadas realizadas.
+    def darCostoLlamadas(self):
+        # TODO Parte2 PuntoB: Completar el método según la documentación dada.
+        #Aqui va el codigo 
+        return self.costoLlamadas
+        
+    # Retorna el número de llamadas realizadas por esta línea.
+    def darNumeroLlamadas(self):
+        # TODO Parte2 PuntoC: Completar el método según la documentación dada.
+        #Aqui va el codigo
+        return self.numeroLlamadas
+        
+    # Retorna el número de minutos consumidos.
+    def darNumeroMinutos(self):
+        # TODO Parte2 PuntoD: Completar el método según la documentación dada.
+        return self.numeroMinutos
+
+    # Reinicia la línea telefónica, dejando todos sus valores en cero.
+    # post: El número de llamadas, número de minutos y costo de llamadas son 0.
+    def reiniciar(self):
+        # TODO Parte2 PuntoE: Completar el método según la documentación dada.
+        LineaTelefonica = 0
+
+
+    # Agrega una llamada local a la línea telefónica
+    # post: Se incrementá en 1 numeroDeLlamadas, se incremento numeroDeMinutos en minutos, costoLlamadas aumentá en ( minutos * 35 ).
+    # :param pMinutos Número de minutos de la llamada. pMinutos >0.
+    def agregarLlamadaLocal(self, pMinutos):
+        
+        # Una llamada más
+        self.numeroLlamadas += 1
+        # Suma los minutos consumidos
+        self.numeroMinutos += pMinutos
+        # Suma el costo (costo por minuto: 35 pesos)
+        self.costoLlamadas += pMinutos * 35
+
+    """
+        Agrega una llamada de larga distancia a la línea telefónica.
+        
+        post: Se incrementá en 1 numeroDeLlamadas, se incremento numeroDeMinutos en minutos, costoLlamadas aumentá en ( minutos * 380 )
+        
+        :param pMinutos: Número de minutos de la llamada. pMinutos >0.
+        """
+
+    def agregarLlamadaLargaDistancia(self, pMinutos):
+        # TODO Parte2 PuntoF: Completar el método según la documentación dada.
+
+        # Una llamada más
+        self.numeroLlamadas += 1
+        # Suma los minutos consumidos
+        self.numeroMinutos += pMinutos
+        # Suma el costo (costo por minuto: 380 pesos)
+        self.costoLlamadas += pMinutos * 380
+
+    '''
+        Agrega una llamada a celular a la lÍnea telefónica
+        post: Se incrementá en 1 numeroDeLlamadas, se incremento numeroDeMinutos en minutos, costoLlamadas aumentá en ( minutos * 999 )
+        :param pMinutos Número de minutos de la llamada. pMinutos >0.
+    '''
+    def agregarLlamadaCelular(self, pMinutos):
+        # TODO Parte2 PuntoG: Completar el método según la documentación dada.
+           
+        # Una llamada más
+        self.numeroLlamadas += 1
+        # Suma los minutos consumidos
+        self.numeroMinutos += pMinutos
+        # Suma el costo (costo por minuto: 999 pesos)
+        self.costoLlamadas += pMinutos * 999
